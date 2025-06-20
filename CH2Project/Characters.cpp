edf281257944e06a991bd65a7baf65cee13239da
hakunamatata9909 <jkichul35@gmail.com>
2025-06-20 16:58:41 +0900

    주석 다 지우기

--- CH2Project/Characters.cpp
+++ CH2Project/Characters.cpp
@@ -71,5 +71,3 @@ void Characters::SetExperience(int newExp) { Experience = newExp; }
 int Characters::GetGold() const { return Gold; }
 void Characters::SetGold(int newGold) { Gold = newGold; }
 vector<Item*>& Characters::GetInventory() { return Inventory; }
-//gitpush테스트
-//test브랜치에merge
